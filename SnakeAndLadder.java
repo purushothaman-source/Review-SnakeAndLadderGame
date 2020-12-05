ea0d56a96ff5bb84497c0738723b3e4ccdd7dd5c
purushothaman-source <72643364+purushothaman-source@users.noreply.github.com>
2020-12-05 11:52:42 +0530

    REFRACTORED : Removed extra spaces

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,16 +1,23 @@
 package com.program.snakeandladder;
 
 import java.util.Random;
-
+class SnakeAndLadderGame{
+	public static void random() {
+		Random random =new Random();
+		int rollDie=random.nextInt(7);
+	}
+}
 //MainClass
 public class SnakeAndLadder {
  //mainMethod
 	public static void main(String[] args) {
-		Random random =new Random();
+		
 		//initializing variable playerAPosition and rollDie
 		int playerAPosition=0;
-		int rollDie;
-		rollDie=random.nextInt(7);
+		int player=1;
+		SnakeAndLadderGame.random();
+		
 				
 	}	
 }
+	
