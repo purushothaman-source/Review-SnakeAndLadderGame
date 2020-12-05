1ea583dd62a493259c6c7c725d122148ab92c15f
purushothaman-source <purushoth99jun@gmail.com>
2020-12-05 11:28:52 +0530

    ADDED :initial position of player1

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,12 @@
 package com.program.snakeandladder;
+class SnakeAndLadderGame{
+        //Initializing variable playerA and rollDie
+        int playerA=0;
+        int rollDie;
+}
 //MainClass
 public class SnakeAndLadder {
  //mainMethod
         public static void main(String[] args) {
-                System.out.print("WELCOME TO SNAKE AND LADDER GAME")
         }
 }
